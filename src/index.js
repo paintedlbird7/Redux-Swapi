54c02ce9005a051ed5c8e3e04f1939308df8e966
paintedlbird7 <rosaperezlarios@gmail.com>
2019-03-13 10:55:16 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
+import { createStore, applyMiddleware, compose } from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+// import logger from './logger';
 // rootReducer from ./reducers
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  //   /* applyMiddleware goes here */
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
+
+// const store = createStore(
+//   rootReducer
+//   /* applyMiddleware goes here */
+  
+// );
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,47 @@
-import /* we need our action types here*/ "../actions";
+import actions from /* we need our action types here*/ "../actions";
+
 const initialState = {
   characters: []
   // Array characters, Boolean fetching, null error.
+  , isLoading: false,
+  photoOfTheDay: null,
+  error: ''
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_PHOTO_START:
+    // STEP IV - Handle the START action - it should return the new state
+    // tree with isLoading: true, so that our UI transitions to the loading
+    // state (Don't forget to add an isLoding bool to the initial state tree)
+    return {
+      ...state,
+      error: '',
+      isLoading: true
+    };
+  case FETCH_PHOTO_SUCCESS:
+    // STEP VII - Handle the SUCCESS action - it should return the new state
+    // tree with isLoading: false, so that our UI transitions from the loading
+    // state to the success state
+    return {
+      ...state,
+      error: '',
+      isLoading: false,
+      photoOfTheDay: action.payload
+    };
+  case FETCH_PHOTO_FAILURE:
+    // STEP XI - Handle the SUCCESS action - it should return the new state
+    // tree with isLoading: false, so that our UI transitions from the loading
+    // state to the success state (The UI was already built for this, so
+    // now we're good to to!)
+    return {
+      ...state,
+      isLoading: false,
+      error: action.payload
+    };
     default:
       return state;
   }
